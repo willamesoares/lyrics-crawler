8fcdf31a733789b7a51f6b4a47e3a129bf973c1e
Will Soares <willame_soares@hotmail.com>
2017-07-29 14:08:31 -0300

    use bs4 instead of lxml for page scraping

--- get-lyric.py
+++ get-lyric.py
@@ -1,35 +1,38 @@
 import sys, dbus, requests
-from lxml import html
+from bs4 import BeautifulSoup
 
 defaults = {
-    "request": {
-        "token": 'Kz0wSeMnCT7kgo6jGPltZ9CrsSl2OjRbF8GR4HLX6GayoE07G1GQiciXasFpImGt',
-        "base_url": "https://api.genius.com"
+    'request': {
+        'token': '',
+        'base_url': 'https://api.genius.com'
     },
-    "message": {
-        "search_fail": "The lyrics for this song were not found!",
-        "wrong_input": "Wrong number of arguments.\n" \
-                       "Use two parameters to perform a custom search " \
-                       "or none to get the song currently playing on Spotify."
+    'message': {
+        'search_fail': 'The lyrics for this song were not found!',
+        'wrong_input': 'Wrong number of arguments.\n' \
+                       'Use two parameters to perform a custom search ' \
+                       'or none to get the song currently playing on Spotify.'
     }
-
 }
+
+def get_token():
+    f = open('token.txt', 'r')
+    defaults['request']['token'] = f.readline().rstrip()
 
 def get_current_song_info():
     # kudos to jooon at stackoverflow http://stackoverflow.com/a/33923095
     session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify",
-                                         "/org/mpris/MediaPlayer2")
+    spotify_bus = session_bus.get_object('org.mpris.MediaPlayer2.spotify',
+                                         '/org/mpris/MediaPlayer2')
     spotify_properties = dbus.Interface(spotify_bus,
-                                        "org.freedesktop.DBus.Properties")
-    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+                                        'org.freedesktop.DBus.Properties')
+    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
     return {'artist': metadata['xesam:artist'][0], 'title': metadata['xesam:title']}
 
 def request_song_info(song_title, artist_name):
-    base_url = defaults["request"]["base_url"]
-    headers = {'Authorization': 'Bearer ' + defaults["request"]["token"]}
-    search_url = base_url + "/search"
+    base_url = defaults['request']['base_url']
+    headers = {'Authorization': 'Bearer ' + defaults['request']['token']}
+    search_url = base_url + '/search'
     data = {'q': song_title + ' ' + artist_name}
     response = requests.get(search_url, data=data, headers=headers)
 
@@ -37,26 +40,28 @@ def request_song_info(song_title, artist_name):
 
 def scrap_song_url(url):
     page = requests.get(url)
-    tree = html.fromstring(page.content)
-
-    lyrics = tree.xpath('.//lyrics//text()')
+    html = BeautifulSoup(page.text, 'html.parser')
+    [h.extract() for h in html('script')]
+    lyrics = html.find('div', class_='lyrics').get_text()
 
     return lyrics
 
 def main():
     args_length = len(sys.argv)
 
+    get_token()
+
     if args_length == 1:
         # Get info about song currently playing on Spotify
         current_song_info = get_current_song_info()
-        song_title = current_song_info["title"]
-        artist_name = current_song_info["artist"]
+        song_title = current_song_info['title']
+        artist_name = current_song_info['artist']
     elif args_length == 3:
         # Use input as song title and artist name
         song_info = sys.argv
         song_title, artist_name = song_info[1], song_info[2]
     else:
-        print(defaults["message"]["wrong_input"])
+        print(defaults['message']['wrong_input'])
         return
 
     print('{} by {}'.format(song_title, artist_name))
@@ -66,18 +71,27 @@ def main():
     json = response.json()
     remote_song_info = None
 
-    for hit in json["response"]["hits"]:
-        if artist_name.lower() in hit["result"]["primary_artist"]["name"].lower():
+    for hit in json['response']['hits']:
+        if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
             remote_song_info = hit
             break
 
     # Extract lyrics from URL if song was found
     if remote_song_info:
-        song_url = remote_song_info["result"]["url"]
+        song_url = remote_song_info['result']['url']
         lyrics = scrap_song_url(song_url)
-        print("\n".join(lyrics))
+
+        write_lyrics_to_file(lyrics, song_title, artist_name)
+
+        print(lyrics)
     else:
-        print(defaults["message"]["search_fail"])
+        print(defaults['message']['search_fail'])
+
+def write_lyrics_to_file (lyrics, song, artist):
+    f = open('lyric-view.txt', 'w')
+    f.write('{} by {}'.format(song, artist))
+    f.write(lyrics)
+    f.close()
 
 if __name__ == '__main__':
     main()
